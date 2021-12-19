bdab56c59f2366feedcd4157527682ae9c6161c2
Mani <manikumar.panithi@gmail.com>
2021-12-19 22:33:31 +0530

    First commit

---
+++ src/main/java/com/cigna/JavaStreamSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cigna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStreamSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cigna/JavaStreamSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cigna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
