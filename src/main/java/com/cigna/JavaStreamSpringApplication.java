a83f8e7e14e255d72861709b623f75b82ad60f37
Mani <manikumar.panithi@gmail.com>
2024-03-04 08:50:21 -0500

    Task_124:main method change

--- src/main/java/com/cigna/JavaStreamSpringApplication.java
+++ src/main/java/com/cigna/JavaStreamSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaStreamSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hi Git : 04-03-24");
 		SpringApplication.run(JavaStreamSpringApplication.class, args);
 	}
 
